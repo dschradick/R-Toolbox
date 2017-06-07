f55190b7dd655270551cfc62f6c1bbdc37bd5c10
Daniel Schradick <danielschradick@Macintosh-2.fritz.box>
2017-06-07 22:12:54 +0200

    Initial commit

---
+++ src/main/java/ds/testapp/MyClass.java
@@ -0,0 +1,21 @@
+package ds.testapp;
+
+/**
+ * Created by danielschradick on 07.06.17.
+ */
+public class MyClass {
+
+    private int number;
+
+    public MyClass() {
+        number = 10;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
